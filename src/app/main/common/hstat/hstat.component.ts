537567f71498b3eaff3d818eaa48e25a89ba694b
lisandr84 <lisandr84@gmail.com>
2019-03-01 19:31:45 +0100

    2.0.0.4-beta

--- src/app/main/common/hstat/hstat.component.ts
+++ src/app/main/common/hstat/hstat.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, AfterViewInit, SimpleChange } from '@angular/core';
+import { Component, Input, SimpleChange } from '@angular/core';
 
 @Component({
   selector: 'tg-hstat',
