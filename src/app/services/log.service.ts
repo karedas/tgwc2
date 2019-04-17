f80317d7508780c53526174cdfe49b4c57675f3e
karedas84 <lisandr84@gmail.com>
2019-04-17 17:13:43 +0200

    aggiunto log service

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -37,9 +37,71 @@ export class LogService {
     data = data.replace(/&!cmdlst\{[\s\S]*?\}!/gm, '');
 
 
+    // Generic page (title, text)
+    data = data.replace(/&!page\{[\s\S]*?\}!/gm, (p) => {
+      p = JSON.parse(p.slice(6, -1));
+      return this.printPage(p)
+    });
 
+    // Generic table (title, head, data)
+    data = data.replace(/&!table\{[\s\S]*?\}!/gm, (t) => {
+      t = JSON.parse(t.slice(7, -1));
+      return this.printTable(t);
+    });
 
+    // Room details
+    data = data.replace(/&!room\{[\s\S]*?\}!/gm, (dtls) => {
+      dtls = JSON.parse(dtls.slice(6, -1));
+      return this.printDetails(dtls, 'room');
+    });
 
+    // Person details
+    data = data.replace(/&!pers\{[\s\S]*?\}!/gm, (dtls) => {
+      dtls = JSON.parse(dtls.slice(6, -1));
+      return this.printDetails(dtls, 'pers');
+    });
+
+    // Object details
+    data = data.replace(/&!obj\{[\s\S]*?\}!/gm, (dtls) => {
+      dtls = JSON.parse(dtls.slice(5, -1).replace(/\n/gm, ' '));
+      return this.printDetails(dtls, 'obj');
+    });
+
+    // Inventory
+    data = data.replace(/&!inv\{[\s\S]*?\}!/gm, (inv) => {
+      inv = JSON.parse(inv.slice(5, -1));
+      return this.printInventory(inv);
+    });
+
+    // Equipment
+    data = data.replace(/&!equip\{[\s\S]*?\}!/gm, (eq) => {
+      eq = JSON.parse(eq.slice(7, -1).replace(/\n/gm, '<br>'));
+      return this.printEquipment(eq);
+    });
+
+    // Workable lists
+    data = data.replace(/&!wklst\{[\s\S]*?\}!/gm, (wk) => {
+      wk = JSON.parse(wk.slice(7, -1));
+      return this.printWorksList(wk);
+    });
+
+    // Skill list
+    data = data.replace(/&!sklst\{[\s\S]*?\}!/gm, (skinfo) => {
+      skinfo = JSON.parse(skinfo.slice(7, -1));
+      return this.printSkillsList(skinfo);
+    });
+
+    // Player info
+    data = data.replace(/&!pginf\{[\s\S]*?\}!/gm, (info) => {
+      info = JSON.parse(info.slice(7, -1));
+      return this.printPlayerInfo(info);
+    });
+
+    // Player status
+    data = data.replace(/&!pgst\{[\s\S]*?\}!/gm, (status) => {
+      status = JSON.parse(status.slice(6, -1));
+      return this.printPlayerStatus(status);
+    });
 
 
     // Refresh
@@ -52,22 +114,22 @@ export class LogService {
     data = data.replace(/&\*/gm, '');
 
 
-    data = data.replace(/&!m"(.*)"\{([\s\S]*?)\}!/gm, (line: any ,type: any ,data: any ) => {
+    data = data.replace(/&!m"(.*)"\{([\s\S]*?)\}!/gm, (line: any, type: any, data: any) => {
       return data;
     });
-  
+
     data = data.replace(/&!ce"[^"]*"/gm, '');
-  
+
     data = data.replace(/&!ulink"[^"]*"/gm, (link) => {
       var link = link.slice(8, -1).split(',');
       return link[1];
     });
-  
+
     data = data.replace(/&(i|I\d)/gm, '');
-  
+
     data = data.replace(/\n/gm, '<br>');
 
-    data = '<p>'+ this.removeColors(data)+'</p>';
+    data = '<p>' + this.removeColors(data) + '</p>';
 
     console.log(data)
 
@@ -81,4 +143,22 @@ export class LogService {
   private printPrompt(st) {
     return '';
   }
+
+  private printPage(page) {}
+
+  private printTable(page) {}
+
+  private printDetails(detail, type) {}
+
+  private printInventory(inv) {}
+
+  private printEquipment(eq) {}
+
+  private printWorksList(wklist) {}
+
+  private printSkillsList(skills) {}
+
+  private printPlayerInfo(pinfo) {}
+
+  private printPlayerStatus(status) {}
 }
