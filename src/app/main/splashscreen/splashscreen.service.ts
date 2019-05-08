e7a10f11b56e506eddfa78ce4bd260af899b941f
karedas84 <lisandr84@gmail.com>
2019-05-08 09:32:24 +0200

    new character

--- src/app/main/splashscreen/splashscreen.service.ts
+++ src/app/main/splashscreen/splashscreen.service.ts
@@ -12,26 +12,27 @@ export class SplashScreenService {
   private totalAssets: number;
   public status$: BehaviorSubject<boolean>;
 
-  public percentage$: BehaviorSubject<any> =  new BehaviorSubject<any>(0);
+  public percentage$: BehaviorSubject<any> = new BehaviorSubject<any>(0);
 
   constructor() {
+    
     this.status$ = new BehaviorSubject<boolean>(false);
 
     this.totalAssets = AssetsList.length;
 
     AssetsList.forEach((urlimg: string, i: any) => {
-        this.images[i] = new Image();
-        this.images[i].src = 'assets/images/' + urlimg;
-        this.images[i].onload = () => {
+      this.images[i] = new Image();
+      this.images[i].src = 'assets/images/' + urlimg;
+      this.images[i].onload = () => {
         this.percentage$.next(this.percentage$.value);
         this.imageLoadPost();
-        };
+      };
 
-        this.images[i].onerror = () => {
+      this.images[i].onerror = () => {
 
-          this.percentage$.next(this.percentage$.value);
-          this.imageLoadPost();
-        };
+        this.percentage$.next(this.percentage$.value);
+        this.imageLoadPost();
+      };
     });
   }
 
@@ -43,14 +44,14 @@ export class SplashScreenService {
       this.onComplete();
     }
 
-    this.percentage$.next(this.percentage$.value >= 100 ? 100 : this.percentage$.value  + 1 );
+    this.percentage$.next(this.percentage$.value >= 100 ? 100 : this.percentage$.value + 1);
   }
 
   get percentage() {
     return this.percentage$.asObservable();
   }
 
-  onComplete () {
+  onComplete() {
     this.status$.next(true);
     this.status$.complete();
   }
