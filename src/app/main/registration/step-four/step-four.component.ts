f7bf1e52662052ae3fa20f6061449b7c863a8f28
karedas84 <lisandr84@gmail.com>
2019-05-10 17:15:58 +0200

    new character

--- src/app/main/registration/step-four/step-four.component.ts
+++ src/app/main/registration/step-four/step-four.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { IStats } from '../models/creation_data.model';
 import { HttpClient } from '@angular/common/http';
-import { MatSliderChange } from '@angular/material';
 
 @Component({
   selector: 'tg-step-four',
@@ -28,76 +27,58 @@ export class StepFourComponent implements OnInit {
     willpower: 0
   }
 
-  // Sliders Configuration
-  readonly autoTicks = false;
-  readonly max = 80;
-  readonly min = 20;
-  readonly step = 10;
-  readonly thumbLabel = false;
-  readonly value = 50;
-
   constructor(
     private fb: FormBuilder,
     private http: HttpClient
-  ) { }
+  ) {
+    this.frmStepFour = this.fb.group(this.attributesList);
+  }
 
   ngOnInit() {
-
-    this.frmStepFour = this.fb.group({
-      strength: [0],
-      constitution: [0],
-      size: [0],
-      dexterity: [0],
-      speed: [0],
-      empathy: [0],
-      intelligence: [0],
-      willpower: [0],
-    });
-
-    this.calculateUsedPoints();
-
-    //DEBUG 
-    this.frmStepFour.valueChanges.subscribe((val) => {
-      console.log(val);
-    })
-
+    this.points = this.calculateUsedPoints();
+    console.log(this.points);
   }
 
   private calculateUsedPoints() {
     let sum = 0;
-    let stats = this.frmStepFour.getRawValue();
-    Object.keys(stats).map(e => {
-      sum -= this.statCost(stats[e]);
+
+    Object.keys(this.attributesList).map(e => {
+      sum -= this.statCost(this.attributesList[e]);
     });
-    this.points = sum;
+    return sum;
   }
 
-  private statCost(val: number) {
-    console.log(val);
-    const cost = [-40, -30, -20, -15, -10, -5, 0, 5, 10, 15, 25, 40, 60];
-    const idx = (30 + val) / 5;
+  private statCost(val) {
+    const cost = [-40, -20, 0, 30, 60];
+    const idx = (2 + val)  ;
     return cost[idx];
   }
 
-  increaseAttr(event: any, id?: any) {
-    if(this.attributesList[id] < 2 ) {
-      this.attributesList[id]++;
-      this.frmStepFour.controls[id].setValue(this.attributesList[id]);
-      this.calculateUsedPoints();
-    }
+  private verifyAttr() {
+    return this.calculateUsedPoints() >= 0;
   }
 
-  decreaseAttr(event: any, id?:any) {
-    if(this.attributesList[id] > -2 ) {
-      this.attributesList[id]--;
-      this.frmStepFour.controls[id].setValue(this.attributesList[id]);
-      this.calculateUsedPoints();
+  increaseAttr(event: any, id?: any) {
+    if (this.attributesList[id] < 2) {
+      this.attributesList[id]= ++this.attributesList[id];
+      this.frmStepFour.controls[id].setValue(this.statCost(this.attributesList[id] * 2));
+      this.points = this.calculateUsedPoints();
+    }
+
+  }
+
+  decreaseAttr(event: any, id?: any) {
+    if (this.attributesList[id] > -2) {
+      this.attributesList[id] = --this.attributesList[id];
+      this.frmStepFour.controls[id].setValue(this.statCost(this.attributesList[id] / 2));
+      this.points = this.calculateUsedPoints();
+      console.log(this.points);
     }
   }
 
   //on Mouse Over, showing description
   onAttribute(event: any, fileName: string) {
-    if(!fileName) {
+    if (!fileName) {
       this.attributeHover = false;
       return;
     }
@@ -107,5 +88,5 @@ export class StepFourComponent implements OnInit {
         this.attributeDesc = data;
         this.attributeHover = true;
       });
-    }
+  }
 }
--- src/assets/data/ethnicity/ethnicity.const.ts
+++ src/assets/data/ethnicity/ethnicity.const.ts
@@ -1,25 +1,25 @@
 export const ethnicity = {
   human: [
-    { name: "Eliantiriano", code: "uma", help_url: 'eliantiriano' },
-    { name: "Emeriano", code: "ume", help_url: 'emeriano' }
+    { name: "Eliantiriano", code: "uma", help_url: 'eliantiriano.html' },
+    { name: "Emeriano", code: "ume", help_url: 'emeriano.html' }
   ],
   halfling: [
-    { name: "Halfling dell'Arcipelago", code: "hal", help_url: 'halfling_arcipelago' }
+    { name: "Halfling dell'Arcipelago", code: "hal", help_url: 'halfling_arcipelago.html' }
   ],
   dwarf: [
-    { name: "Nano Zanarquon", code: "nan", help_url: 'nano_zanarquon' }
+    { name: "Nano Zanarquon", code: "nan", help_url: 'nano_zanarquon.html' }
   ],
   elf: [
-    { name: "Elfo di Alwenion", code: "eal", help_url: 'elfo_alwenion' },
-    { name: "Elfo sinoriano", code: "esi", help_url: 'elfo_sinoriano' },
-    { name: "Elfo ambrato", code: "dra", help_url: 'elfo_ambrato' },
-    { name: "Ousst'viiryn", code: "drw", help_url: 'ousst_viiryn' },
-    { name: "Selvaggio delle radure", code: "els", help_url: 'elfo_selvaggio', limited: true }
+    { name: "Elfo di Alwenion", code: "eal", help_url: 'elfo_alwenion.html' },
+    { name: "Elfo sinoriano", code: "esi", help_url: 'elfo_sinoriano.html' },
+    { name: "Elfo ambrato", code: "dra", help_url: 'elfo_ambrato.html' },
+    { name: "Ousst'viiryn", code: "drw", help_url: 'ousst_viiryn.html' },
+    { name: "Selvaggio delle radure", code: "els", help_url: 'elfo_selvaggio.html', limited: true }
   ],
   goblin: [
-    { name: "Pelleverde", code: "gob", help_url: 'goblin_pelleverde' }
+    { name: "Pelleverde", code: "gob", help_url: 'goblin_pelleverde.html' }
   ],
   orc: [
-    { name: "Nktama", code: "orc", help_url: 'orco_nktama' }
+    { name: "Nktama", code: "orc", help_url: 'orco_nktama.html' }
   ]
 }
