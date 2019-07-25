cbc5d49c7607c9a405c35e8deb5bfe1bdc60527b
karedas84 <lisandr84@gmail.com>
2019-07-25 16:59:23 +0200

    up

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -8,6 +8,7 @@ import { ApiService } from './services/api.service';
 import { UserService } from './services/user.service';
 
 import * as Sentry from '@sentry/browser';
+import { environment } from 'src/environments/environment';
 
 Sentry.init({
   dsn: 'https://4703a40d72734be383834a9100b66168@sentry.io/1511377'
@@ -17,8 +18,9 @@ Sentry.init({
 export class SentryErrorHandler implements ErrorHandler {
   constructor() {}
   handleError(error) {
-    console.log(error);
-    const eventId = Sentry.captureException(error.originalError || error);
+    if (environment.production) {
+      const eventId = Sentry.captureException(error.originalError || error);
+    }
     // Sentry.showReportDialog({ eventId });
   }
 }
