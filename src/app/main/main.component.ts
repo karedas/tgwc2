bcea548408d381b7a94d087663d7b8c2eeff39d9
karedas84 <lisandr84@gmail.com>
2019-08-14 11:45:08 +0200

    refactoring cookielaw experience + some button stylesheet changes.

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,6 +3,7 @@ import { Subject } from 'rxjs';
 import { CookieService } from 'ngx-cookie-service';
 import { MatDialog, MatDialogConfig, MatDialogRef } from '@angular/material/dialog';
 import { CookieLawComponent } from './common/components/dialogs/cookie-law/cookie-law.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'tg-main',
@@ -20,39 +21,43 @@ export class MainComponent implements OnInit, OnDestroy {
   private _unsubscribeAll = new Subject();
 
   constructor(
+    private router: Router,
     private cookieService: CookieService,
     public dialog: MatDialog,
   ) {
-
     this._unsubscribeAll = new Subject();
   }
 
   ngOnInit(): void {
 
-    if (!this.cookieService.check('tgCookieLaw')) {
-      this.openCookieLaw().afterClosed()
-        .subscribe(() => {
-          this.start();
-        });
-    } else {
+    if (this.cookieService.check('tgCookieLaw')) {
+      this.router.navigate(['/manager']);
       this.start();
+    } else {
+      this.openCookieLaw()
     }
   }
 
 
-  openCookieLaw(): MatDialogRef<CookieLawComponent, MatDialogConfig> {
+  openCookieLaw() {
+
     const dialogID = 'cookielaw';
     const config = new MatDialogConfig();
     config.id = dialogID;
     config.disableClose = true;
     config.autoFocus = false;
     config.width = '450px';
-    config.height = '250px';
+    config.height = '350px';
     const dialogRef = this.dialog.open(CookieLawComponent, config);
-    return dialogRef;
+
+    dialogRef.afterClosed()
+      .subscribe(() => {
+        this.router.navigate(['/manager']);
+        this.start();
+      });
   }
 
-   onCookieAccepted(status: boolean) {
+  onCookieAccepted(status: boolean) {
     this.isCookieAccepted = status;
   }
 
