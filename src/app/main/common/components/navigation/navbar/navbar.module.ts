ba054ea8c42fab83e35f861771e480ea8a003be4
karedas84 <lisandr84@gmail.com>
2019-08-21 11:43:50 +0200

    Fix wrong file name.

--- src/app/main/common/components/navigation/navbar/navbar.module.ts
+++ src/app/main/common/components/navigation/navbar/navbar.module.ts
@@ -4,7 +4,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { MatBadgeModule, MatToolbarModule, MatListModule } from '@angular/material';
 import { NavBarComponent } from './navbar.component';
-import { TgGameItemsComponent } from './game-items/game-itemscomponent';
+import { TgGameItemsComponent } from './game-items/game-itemsc.omponent';
 
 
 @NgModule({
