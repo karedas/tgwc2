4354a2fa14cb1342b4da0b02f7a6843ba8749933
lisandr84 <lisandr84@gmail.com>
2019-08-27 00:53:44 +0200

    some work in status-bar and fix

--- src/app/main/client/components/status-bar/status-bar.component.ts
+++ src/app/main/client/components/status-bar/status-bar.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { TGConfig } from '../../client-config';
+import { ConfigService } from 'src/app/services/config.service';
+import { Subject, Observable } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { hero_position } from '../../common/constants';
+import { getHero } from '../../store/selectors';
+import { Store, select } from '@ngrx/store';
+import { DataState } from '../../store/state/data.state';
 
 @Component({
   selector: 'tg-status-bar',
   templateUrl: './status-bar.component.html',
   styleUrls: ['./status-bar.component.scss']
 })
-export class StatusBarComponent implements OnInit {
+export class StatusBarComponent implements OnInit, OnDestroy  {
 
-  constructor() { }
-
-  ngOnInit() {
+  tgConfig: TGConfig;
+  hero_pos = hero_position;
+  hero$: Observable<any>;
+  private _unsubscribeAll: Subject<any>;
+  
+  constructor(
+    private store: Store<DataState>,
+    private _configService: ConfigService
+  ) { 
+    this.hero$ = this.store.pipe(select(getHero));
+    
+    this._unsubscribeAll = new Subject();
   }
 
+  ngOnInit() {
+    // Subscribe to config changes
+    this._configService.config
+      .pipe(takeUntil(this._unsubscribeAll))
+      .subscribe((config) => {
+        this.tgConfig = config;
+      });
+  }
+
+  ngOnDestroy() {
+    this._unsubscribeAll.next();
+    this._unsubscribeAll.complete();
+  }
 }
