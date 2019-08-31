dc53fe04cacec7e72262ebe34401b9f871bae94a
lisandr84 <lisandr84@gmail.com>
2019-09-01 00:19:21 +0200

    big changes (no desc)

--- src/app/main/client/components/status-bar/status-bar.component.ts
+++ src/app/main/client/components/status-bar/status-bar.component.ts
@@ -3,10 +3,11 @@ import { TGConfig } from '../../client-config';
 import { ConfigService } from 'src/app/services/config.service';
 import { Subject, Observable } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
-import { hero_position } from '../../common/constants';
+import { hero_position, hero_attitude, hero_defense, hero_speed } from '../../common/constants';
 import { getHero } from '../../store/selectors';
 import { Store, select } from '@ngrx/store';
 import { DataState } from '../../store/state/data.state';
+import { GameService } from '../../services/game.service';
 
 @Component({
   selector: 'tg-status-bar',
@@ -16,12 +17,20 @@ import { DataState } from '../../store/state/data.state';
 export class StatusBarComponent implements OnInit, OnDestroy  {
 
   tgConfig: TGConfig;
-  hero_pos = hero_position;
   hero$: Observable<any>;
+  
+  readonly speedValue = hero_speed;
+  readonly positionValue = hero_position;
+  readonly attitudeValue = hero_attitude;
+  readonly defenseValue = hero_defense;
+
+
+
   private _unsubscribeAll: Subject<any>;
   
   constructor(
     private store: Store<DataState>,
+    private gameService: GameService,
     private _configService: ConfigService
   ) { 
     this.hero$ = this.store.pipe(select(getHero));
@@ -36,6 +45,11 @@ export class StatusBarComponent implements OnInit, OnDestroy  {
       .subscribe((config) => {
         this.tgConfig = config;
       });
+  }
+
+  onClickButton(what: string, param?: string) {
+    param = param ? ' ' + param.substring(0, 4) : '';
+    this.gameService.processCommands(what + param);
   }
 
   ngOnDestroy() {
