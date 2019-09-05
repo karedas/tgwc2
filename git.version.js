960d6ba8e12edbea8751281b50406fcbdac55a77
lisandr84 <lisandr84@gmail.com>
2019-09-05 22:47:23 +0200

    Merge branch '2.0.1.0'

--- git.version.js
+++ git.version.js
@@ -1,10 +1,15 @@
 
-const { gitDescribeSync } = require('git-describe');
+const { gitDescribe, gitDescribeSync } = require('git-describe');
 const { writeFileSync } = require('fs');
 const path = require('path');
+
+ 
+// Target working directory
+const gitInfo = gitDescribeSync();
 
 const info = gitDescribeSync(__dirname, {
 });
 const infoJson = JSON.stringify(info, null, 2);
 
+console.log(gitInfo);
 writeFileSync(path.join(__dirname, '/src/git-version.json'), infoJson);
