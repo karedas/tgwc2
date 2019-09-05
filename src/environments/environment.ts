459299aaf976abadf8397db3264e96fc69fda9dc
lisandr84 <lisandr84@gmail.com>
2019-09-05 22:40:40 +0200

    Merge branch '2.0.1.0'

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,20 @@
 export const environment = {
-  production: true,
-  apiAddress: 'http://http://play.thegatemud.it/api',
+  production: false,
+  name: '',
+  apiAddress: 'http://localhost:9595/api',
   serverstatAddress: 'http://play.thegatemud.it/serverstat',
   media_address: 'http://play.thegatemud.it/images/',
-  analytics_UAID: 'UA-122402371-1',
+  analytics_UAID: 'UA-122402371-2',
   socket : {
-    url : 'http://play.thegatemud.it:3335',
+    url : 'http://51.38.185.84:3335',
     options : {
       reconnection: true,
       autoConnect: true,
       forceNew: false,
       resource: 'socket.io',
       transports: ['websocket'],
-      reconnectionDelay: 1500,
+      reconnectionDelay: 3000,
       reconnectionDelayMax : 5000,
     }
-  },
+  }
 };
