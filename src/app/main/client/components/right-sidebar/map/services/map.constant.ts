9b8bd0143c61c6e06c0e5c903282c8bc875eea63
lisandr84 <lisandr84@gmail.com>
2019-10-15 21:49:40 +0200

    added new shadow environment

---
+++ src/app/main/client/components/right-sidebar/map/services/map.constant.ts
@@ -0,0 +1,6 @@
+export const terrainsCastShadow = [
+    45, 46, 47, 50, 51, 52, 53, 54, 60
+  ]
+  export const terrainsHasShadow = [
+    1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,31,41,43,44
+  ]
